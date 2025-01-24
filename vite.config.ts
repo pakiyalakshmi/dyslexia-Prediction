25ea771cdb9da8246364f152c24e3c1bee7ed1bc
pakiyalakshmi <134521823+pakiyalakshmi@users.noreply.github.com>
2025-01-24 22:25:32 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
 export default defineConfig({
   base: '/dyslexia-Prediction/',  // Make sure this is the name of your repository
   plugins: [react()],
